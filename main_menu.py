b4830883eba92b6a4739fa048281d71510ec4f48
baldash <hugo.baldassin@gmail.com>
2025-11-10 11:27:54 +0100

    [EDIT] started working on side menu

--- main_menu.py
+++ main_menu.py
@@ -3,12 +3,12 @@ from params import *
 import game_of_life as gol
 import edit_map as editor
 
-play_button_shape = Rectangle((int)(WINDOW_SIZE / 2) - 80 / 2, (int)(WINDOW_SIZE / 2) - 100, 80, 40)
-edit_button_shape = Rectangle((int)(WINDOW_SIZE / 2) - 80 / 2, (int)(WINDOW_SIZE / 2), 80, 40)
-exit_button_shape = Rectangle((int)(WINDOW_SIZE / 2) - 80 / 2, (int)(WINDOW_SIZE / 2) + 100, 80, 40)
+play_button_shape = Rectangle((int)((WINDOW_SIZE  + MENU_SIZE) / 2) - 80 / 2, (int)(WINDOW_SIZE / 2) - 100, 80, 40)
+edit_button_shape = Rectangle((int)((WINDOW_SIZE  + MENU_SIZE) / 2) - 80 / 2, (int)(WINDOW_SIZE / 2), 80, 40)
+exit_button_shape = Rectangle((int)((WINDOW_SIZE  + MENU_SIZE) / 2) - 80 / 2, (int)(WINDOW_SIZE / 2) + 100, 80, 40)
 exit = False
 
-init_window(WINDOW_SIZE, WINDOW_SIZE, WINDOW_TITLE)
+init_window(MENU_SIZE + WINDOW_SIZE, WINDOW_SIZE, WINDOW_TITLE)
 while not window_should_close() and not exit:
     begin_drawing()
     clear_background(BLACK)
